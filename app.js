017c57160626807c73acc8da872ad10d66846b75
Arthur <arthur.albert990@gmail.com>
2022-09-13 11:41:37 -0300

    Update display functions

--- app.js
+++ app.js
@@ -29,23 +29,65 @@ function sqrt(a){
     return Math.sqrt(a);
 }
 
-
+//operators array
+const operatorsArray = ['C','+/-','÷','/','×','-','+','=']
 //function to print key on display text
-const display = document.querySelector('.current-operation p')
+const display = document.querySelector('.current-operation p');
+const pastOpDisplay = document.querySelector('.past-operation > p');
+
 function updateDisplay(e){
-    display.innerText += e.innerText;
+    //if only zero at display, clear:
+    if (display.innerText=='0'){
+        display.innerText='';
+    }
+
+    // if operator "C", clear display
+    if (e.innerText=='C'){
+        display.innerText='0';
+        pastOpDisplay.innerText = '';
+        console.log('Clear calculator.')
+        return e;
+    }
+
     
-    if (display.innerText.length > 6){
-        display.style.fontSize = '0.5em';
+    // if key pressed is an operator, show on superior line
+    if (operatorsArray.includes(e.innerText) && e.innerText != '='){
+        if(pastOpDisplay.innerText.includes(e.innerText)){
+            //second operator pressed, makes calc on "memory", calc on pastOpDisplay line  
+            pastOpDisplay.innerText += display.innerText+'=';
+            display.innerText='result';
+
+            //PAREI AQUI:
+            //PAREI AQUI:
+            //PAREI AQUI:
+            console.log('REALIZAR OPERAÇÃO NA MEMÓRIA')
+            //chamar função de cálculo
+            return (e);
+        }
+
+        pastOpDisplay.innerText += display.innerText+e.innerText;
+        display.innerText='0';
+
+    } else if (e.innerText == '='){
+        console.log('REALIZAR OPERAÇÃO')
+    } else {
+        display.innerText += e.innerText; //append new key e.innerText
     }
 
     return e;
-
 }
 
+function updateDisplayFont(e){
+        //adjust display text size
+        if (display.innerText.length > 6){
+            display.style.fontSize = '0.5em';
+        }
+}
 //function to print last operation on display
 
 //function to identify operator and call operator function
+
+//function to identify if it will start a new operation on display
 
 //bind events for calculator keyboard on click
 
@@ -56,10 +98,12 @@ function keyPress(e){
 }
 
 
+
 const keys = document.querySelectorAll('.key');
 
 keys.forEach((element) =>{
     element.addEventListener('click',function(){keyPress(element)})
     element.addEventListener('click',function(){updateDisplay(element)})
+    element.addEventListener('click',function(){updateDisplayFont(element)})
 });
 
