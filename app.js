919718bd93398b6e1d321526c098d01ead1993c1
Arthur <arthur.albert990@gmail.com>
2022-09-14 07:32:12 -0300

    Update functions

--- app.js
+++ app.js
@@ -4,33 +4,28 @@ console.log('js working')
 //  A CADA CARACTERE NOVO DIMINUIR A FONTE PARA NÃO TRANSBORDAR!
 
 //create basic functions for math operations
-// sum
 function sum(a,b){
     return a+b;
 }
-// subtraction
 function subtract(a,b){
     return a-b;
 }
-// divide
 function divide(a,b){
     return a/b;
 }
-// times
-function times(a,b){
+function multiply(a,b){
     return a*b;
 }
-// power
 function power(a,b){
     return a**b; // or Math.pow(a,b)
 }
-// square root
 function sqrt(a){
     return Math.sqrt(a);
 }
 
 //operators array
-const operatorsArray = ['C','+/-','÷','/','×','-','+','=']
+const operatorsArray = ['C','+/-','÷','/','×','-','+','='];
+
 //function to print key on display text
 const display = document.querySelector('.current-operation p');
 const pastOpDisplay = document.querySelector('.past-operation > p');
@@ -55,13 +50,11 @@ function updateDisplay(e){
         if(pastOpDisplay.innerText.includes(e.innerText)){
             //second operator pressed, makes calc on "memory", calc on pastOpDisplay line  
             pastOpDisplay.innerText += display.innerText+'=';
-            display.innerText='result';
 
-            //PAREI AQUI:
-            //PAREI AQUI:
-            //PAREI AQUI:
-            console.log('REALIZAR OPERAÇÃO NA MEMÓRIA')
             //chamar função de cálculo
+            operate(pastOpDisplay.innerText)
+            //exibir resultado
+
             return (e);
         }
 
@@ -70,6 +63,9 @@ function updateDisplay(e){
 
     } else if (e.innerText == '='){
         console.log('REALIZAR OPERAÇÃO')
+        pastOpDisplay.innerText += display.innerText+'=';
+        display.innerText= operate(pastOpDisplay.innerText)
+
     } else {
         display.innerText += e.innerText; //append new key e.innerText
     }
@@ -83,13 +79,22 @@ function updateDisplayFont(e){
             display.style.fontSize = '0.5em';
         }
 }
-//function to print last operation on display
 
 //function to identify operator and call operator function
+function operate(str){
+    let equation = str;
+        //PAREI AQUI:
+        //PAREI AQUI:
+        //PAREI AQUI:
+    equation.replace('/[+/-÷/×-+=]/g','');
+    console.log(`Operate: ${equation}`);
+
+    return ;
+}
 
 //function to identify if it will start a new operation on display
 
-//bind events for calculator keyboard on click
+
 
 //function to process keypress
 function keyPress(e){
@@ -97,8 +102,7 @@ function keyPress(e){
     return this.e;
 }
 
-
-
+//bind events for calculator keyboard on click
 const keys = document.querySelectorAll('.key');
 
 keys.forEach((element) =>{
